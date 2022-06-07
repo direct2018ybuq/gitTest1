fc979d34711b6f84055518efa30d7dfe5dcf8035
tom_glb <goodMorning_glb@direct.com>
2022-06-07 23:50:38 +0800

    Three commit

--- src/main/java/com/direct/TestMain.java
+++ src/main/java/com/direct/TestMain.java
@@ -3,5 +3,6 @@ package com.direct;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
